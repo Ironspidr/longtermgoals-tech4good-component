da28b51fe4c931ef8caba704a49f5f3873a098ef
Ironspidr <63678990+Ironspidr@users.noreply.github.com>
2024-09-08 22:06:52 -0700

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+
+// General Modules
+import { CoreModule } from './core/core.module';
+import { SharedModule } from './shared/shared.module';
+
+// Feature Modules
+import { MainModule } from './main/main.module';
+
+//Heroicons
+import { NgModule } from '@angular/core';
+// import { ModalComponent } from "./app/main/page/modal/modal.components";
+import ModalComponent from "/main/page/modal/modal.components";
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    CoreModule,
+    SharedModule,
+    // Feature Modules
+    MainModule,
+    // Routing Module
+    AppRoutingModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
